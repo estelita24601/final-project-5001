3e278d90f9f35741ae34b58c3e817ce2006406cd
Estelita <estelitac24401@hotmail.com>
2023-11-16 16:14:02 -0800

    working on Task class and testing it

---
+++ task.py
@@ -0,0 +1,77 @@
+from datetime import date
+
+
+class Task:
+    def __init__(self, name, is_complete=False, year=None, month=None, day=None) -> None:
+        """
+        Args:
+            name (str): short description of the task
+            is_complete (bool, optional): Whether the task is complete or not. Defaults to False.
+
+            # if one or more of the following are not provided task will not have a date
+            year (int, optional): 4-digit number for the year. Defaults to None.
+            month (int, optional): 1 or 2-digit number for the month. Defaults to None.
+            day (int, optional): 1 or 2-digit number for the day. Defaults to None.
+        """
+        self.name: str = name
+        self.is_complete = is_complete
+
+        if year and month and day:  # make sure we have everything needed to create a date object
+            self.date = date(year, month, day)
+        else:   # if all three values weren't provided our task doesn't have a date
+            self.date = None
+
+    def __str__(self):
+        output = f"{self.name:10} | {self.date:10} | {self.is_complete}"
+
+    def get_name(self):
+        """returns the name of the task
+
+        Returns:
+            self.name (str)
+        """
+        return self.name
+
+    def get_completion_status(self) -> bool:
+        """returns True if the task is complete and False if it is incomplete
+
+        Returns:
+            self.is_complete(bool)
+        """
+        return self.is_complete
+
+    # QUESTION: Is it okay to return none when there is no date?
+    def get_date(self):
+        """if the task has a due date returns a date object for the due date
+        otherwise returns None
+        """
+        return self.date
+
+    def change_name(self, new_name: str) -> None:
+        """changes the name of the task
+
+        Args:
+            new_name (str): the new name for the task
+
+        """
+        if not isinstance(new_name, str):
+            raise TypeError(self, "new name for the task must be a string")
+        self.name = new_name
+
+    # QUESTION: should I let the date object handle errors for me?
+    def change_date(self, year, month, day):
+        try:
+            self.date = date(year, month, day)
+        except e as TypeError:
+            print(e)
+
+    def change_completion_status(self, new_status: bool) -> None:
+        """updates whether the task is complete or not
+
+        Args:
+            new_status (bool): True for completed tasks and False for incompleted tasks
+        """
+        if not isinstance(new_status, bool):
+            raise TypeError(self, "completion status must be boolean True or False")
+        else:
+            self.is_complete = new_status
---
+++ test_task.py
@@ -0,0 +1,12 @@
+import unittest
+from task import Task
+
+class MyTestCase(unittest.TestCase):
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 4)
