7b4b3c5e034118655f13d38a36c8ee12f2eac400
Estelita <estelitac24401@hotmail.com>
2023-11-29 17:47:00 -0800

    figured out how to use post requests

--- star_trek_prototype/voyager_website.py
+++ star_trek_prototype/voyager_website.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
+    # set up so template can be used
     my_environment = Environment(loader = FileSystemLoader("templates/"))
-    voyager_template = my_environment.get_template("voyager_task_format.html")
+    uss_voyager_template = my_environment.get_template("voyager_task_format.html")
 
-    voyager_list = []
+    # read task names from file into a list
+    uss_voyager_list = []
     with open("fake_task_names.txt", "r") as captains_log:
         for task in captains_log:
             task = task.title()
-            voyager_list.append(task)
+            uss_voyager_list.append(task)
 
-    return voyager_template.render(task_list = voyager_list)
+    if request.method == "POST":
+        form_data = request.form
+        return redirect(url_for("test_form", my_data=form_data))
+
+    # render the template with the list of tasks from the file
+    return uss_voyager_template.render(task_list = uss_voyager_list)
+
+
+@app.route("/testing<my_data>", methods=["POST", "GET"])
+def test_form(my_data):
+    return f"<h1>{my_data}</h1>"
