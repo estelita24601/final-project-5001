277cae69f10c5e991b41924e7dc5d6174fbf0b9c
Estelita <estelitac24401@hotmail.com>
2023-12-05 15:05:30 -0800

    fixed url redirection, need to fix data parsing

--- program_files/website.py
+++ program_files/website.py
@@ -3,74 +3,56 @@ from jinja2 import Environment, FileSystemLoader
 from task import TaskCollection
 
 app = Flask(__name__)
-# read task names from file into a list
+# read task names from file into a list. make it only once for entire program to use
 master_task_collection = TaskCollection("test_input_data.csv")
-
+# set up so template can be used
+my_environment = Environment(loader=FileSystemLoader("templates/"))
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    # set up so template can be used
-    my_environment = Environment(loader=FileSystemLoader("templates/"))
-    task_list_template = my_environment.get_template(
-        "home.html")
+    task_list_template = my_environment.get_template("home.html")
 
-    # if user has interacted with website process data and do what's needed
+    # if user has interacted with website
     if request.method == "POST":
         form_data = request.form.to_dict()
-        user_action = process_post_request(
-            form_data, master_task_collection, "test_output_data.csv")
-
-        # if user_action != "edit": return redirect(url_for("test_form", my_data=form_data)) # FIXME: remove when done with testing
-
+        user_action = process_post_request(form_data, master_task_collection, "test_output_data.csv")
+        
+        if user_action == 'edit':
+            return redirect(url_for("edit_task", post_dict=form_data))
 
     # render the template with the task info from the file
     task_dictionary = master_task_collection.get_task_dictionary()
     return task_list_template.render(task_data=task_dictionary)
 
-
 # FIXME: the entire thing
-@app.route("/edit<task_id>", methods=['POST', 'GET'])
-def edit_task(task_id):
-    my_environment = Environment(loader=FileSystemLoader("templates/"))
+@app.route('/edit/<post_dict>', methods=['POST', 'GET'])
+def edit_task(post_dict):
     task_editor_template = my_environment.get_template(
         "edit_task.html")
 
-    task_to_edit = master_task_collection.get_task(task_id)
-
+    for key in post_dict.keys():
+        task_id = int(key)
+        task_to_edit = master_task_collection.get_task(task_id)
+    
     return task_editor_template.render(task=task_to_edit)
 
 
-def process_post_request(dict_data, task_collection_obj: TaskCollection, csv_file: str):
-    for task_id, action in dict_data.items():
+def process_post_request(post_dict, collection_of_tasks: TaskCollection, csv_file: str):
+    for task_id, action in post_dict.items():
         print("-------ACTION VARIABLE =", action)  # FIXME
         id_number = int(task_id)
 
-        if action == 'delete':
-            task_collection_obj.delete_task(id_number)
-        elif action == 'edit':
-            return redirect(url_for("edit_task", task_id = id_number))
+        if action in ('edit', 'create'):
+            return action
+        elif action == 'delete':
+            collection_of_tasks.delete_task(id_number)
         else:
-            task_obj = task_collection_obj.get_task(id_number)
+            task_obj = collection_of_tasks.get_task(id_number)
             task_obj.change_completion()
             
-    task_collection_obj.update_csv(csv_file)
+    collection_of_tasks.update_csv(csv_file)
 
-        
 
 @app.route("/testing<my_data>", methods=["POST", "GET"])
 def test_form(my_data):
-    return f"<p>{my_data}</p>"
-
-# task_obj = task_collection_obj.get_task(id_number)
-
-# if action == 'delete':
-#     task_collection_obj.delete_task(id_number)
-# elif action == 'edit':
-#     return redirect(url_for("edit_task", task_id=id_number))
-# else:
-#     action = 'checkbox'
-#     task_obj.change_completion()
-
-# task_collection_obj.update_csv(csv_file)
-
-# return action
+    return f"<h1>Testing Page</h1><p>{my_data}</p>"
