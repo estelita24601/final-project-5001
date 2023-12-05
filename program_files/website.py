ed48c2c43718f6fe0454fecd64eafee388fd2b9b
Estelita <estelitac24401@hotmail.com>
2023-12-05 14:09:10 -0800

    broke it by accident - trying to fix

--- program_files/website.py
+++ program_files/website.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 master_task_collection = TaskCollection("test_input_data.csv")
 
 
-@app.route("/home", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def home():
     # set up so template can be used
     my_environment = Environment(loader=FileSystemLoader("templates/"))
@@ -20,22 +20,15 @@ def home():
         user_action = process_post_request(
             form_data, master_task_collection, "test_output_data.csv")
 
-        # if user_action != "edit": return redirect(url_for("test_form", my_data=form_data))
-        # # FIXME: remove when done with testing
+        # if user_action != "edit": return redirect(url_for("test_form", my_data=form_data)) # FIXME: remove when done with testing
 
-    task_dictionary = master_task_collection.get_task_dictionary()
 
     # render the template with the task info from the file
+    task_dictionary = master_task_collection.get_task_dictionary()
     return task_list_template.render(task_data=task_dictionary)
 
 
-@app.route("/testing<my_data>", methods=["POST", "GET"])
-def test_form(my_data):
-    return f"<p>{my_data}</p>"
-
 # FIXME: the entire thing
-
-
 @app.route("/edit<task_id>", methods=['POST', 'GET'])
 def edit_task(task_id):
     my_environment = Environment(loader=FileSystemLoader("templates/"))
@@ -48,20 +41,36 @@ def edit_task(task_id):
 
 
 def process_post_request(dict_data, task_collection_obj: TaskCollection, csv_file: str):
-
     for task_id, action in dict_data.items():
         print("-------ACTION VARIABLE =", action)  # FIXME
         id_number = int(task_id)
-        task_obj = task_collection_obj.get_task(id_number)
 
         if action == 'delete':
             task_collection_obj.delete_task(id_number)
         elif action == 'edit':
-            return redirect(url_for("edit_task", task_id=id_number))
+            return redirect(url_for("edit_task", task_id = id_number))
         else:
-            action = 'checkbox'
+            task_obj = task_collection_obj.get_task(id_number)
             task_obj.change_completion()
+            
+    task_collection_obj.update_csv(csv_file)
 
-        task_collection_obj.update_csv(csv_file)
+        
 
-        return action
+@app.route("/testing<my_data>", methods=["POST", "GET"])
+def test_form(my_data):
+    return f"<p>{my_data}</p>"
+
+# task_obj = task_collection_obj.get_task(id_number)
+
+# if action == 'delete':
+#     task_collection_obj.delete_task(id_number)
+# elif action == 'edit':
+#     return redirect(url_for("edit_task", task_id=id_number))
+# else:
+#     action = 'checkbox'
+#     task_obj.change_completion()
+
+# task_collection_obj.update_csv(csv_file)
+
+# return action
